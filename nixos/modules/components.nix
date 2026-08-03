eca67fbf00adba2fad0b38a48cb801120d8c4d19
Arthur <artm8885@gmail.com>
2026-08-04 02:04:50 +0300

    Add nixos

---
+++ nixos/modules/components.nix
@@ -0,0 +1,71 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    QT_QPA_PLATFORMTHEME = "gtk3";
+  };
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    alacritty
+    chromium
+    git
+    jq
+    libimobiledevice
+    noctalia-shell
+    swaylock
+    wget
+    wl-clipboard
+
+    # Needed for appicons in noctalia
+    tela-icon-theme
+    kora-icon-theme
+    papirus-icon-theme
+    adwaita-icon-theme  # fallback
+    hicolor-icon-theme  # required
+    nwg-look            # theme switcher
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  
+  programs = {
+    # Чтобы zsh был доступен как login shell
+    zsh.enable = true;
+
+    # Нужны права на systemd и /etc
+    amnezia-vpn.enable = true;
+
+    # Регистрирует сессию и настраивает что-то системное
+    niri.enable = true;
+
+    # Нужен для всех пользователей
+    vim.enable = true;
+  };
+
+  users.users.arthr.shell = pkgs.zsh;
+
+  # List services that you want to enable:
+
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
+      user = "greeter";
+    };
+  };
+
+  # For "talking" with iphone
+  services.usbmuxd.enable = true;
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+}
---
+++ nixos/modules/system.nix
@@ -0,0 +1,111 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true;
+  # services.blueman.enable = true; # опционально, GUI для BT
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true; # OR services.tuned.enable
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+  };
+
+  console = {
+    useXkbConfig = true;
+    packages = [ pkgs.terminus_font ];
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us,ru";
+    options = "grp:win_space_toggle";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.arthr = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      dejavu_fonts
+      liberation_ttf
+      nerd-fonts.jetbrains-mono
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      inter
+      material-symbols
+    ];
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" "DejaVu Serif"];
+      sansSerif = [ "Noto Sans" "DejaVu Sans"];
+      monospace = [ "JetBrainsMono Nerd Font"];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+}
+
